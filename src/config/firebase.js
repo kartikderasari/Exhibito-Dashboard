1ebf56dd741b38ed3c2affec57fce6137013ade0
Kartik Derasari <derasarikartik@gmail.com>
2021-04-08 01:23:54 +0530

    Add separate file for Firebase Config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,16 +4,9 @@ import firebase from "firebase/app";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
+import firebaseConfigData from './firebase-config.js'
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDTQswqPtzG6cZFEiNXRlJ983QjFcVAz6o",
-    authDomain: "portfolio-89383.firebaseapp.com",
-    projectId: "portfolio-89383",
-    storageBucket: "portfolio-89383.appspot.com",
-    messagingSenderId: "558205635702",
-    appId: "1:558205635702:web:0eac07a015de22bd2fa357"
-};
+const firebaseConfig = firebaseConfigData;
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
